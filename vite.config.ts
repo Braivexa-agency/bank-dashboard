ea6461829a17baa2dcb57543194f82c7853f28ad
azineddine <amarichezineddine@gmail.com>
2025-10-02 11:52:19 +0100

    refactor(vite.config): increase chunk size warning limit
    - Updated the chunkSizeWarningLimit in vite.config.ts from 5000 to 10000 to accommodate larger builds and reduce unnecessary warnings during the build process.

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     },
   },
   build: {
-    chunkSizeWarningLimit: 5000,
+    chunkSizeWarningLimit: 10000,
     rollupOptions: {
       output: {
         manualChunks(id) {
