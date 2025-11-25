d9d64d62d24d06fae1d11f4ece064d175a9f2c57
azineddine <amarichezineddine@gmail.com>
2025-11-25 16:53:14 +0100

    feat: add authentication check to authenticated routes and redirect unauthenticated users to sign-in

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,6 +1,19 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 import { AuthenticatedLayout } from '@/components/layout/authenticated-layout'
+import { useAuthStore } from '@/stores/authStore'
 
 export const Route = createFileRoute('/_authenticated')({
+  beforeLoad: async ({ location }) => {
+    const { accessToken } = useAuthStore.getState().auth
+    
+    if (!accessToken) {
+      throw redirect({
+        to: '/sign-in',
+        search: {
+          redirect: location.href,
+        },
+      })
+    }
+  },
   component: AuthenticatedLayout,
 })
